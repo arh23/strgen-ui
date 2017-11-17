19aec505ed9b2a1dbe4dc5031613686eb59fae78
arh23 <arh23@users.noreply.github.com>
2017-11-17 10:10:35 +0000

    Added default reference view when none of the toolbar buttons are selected

--- js/elements.js
+++ js/elements.js
@@ -17,46 +17,52 @@ $.fn.extend({
 });
 
 $(document).ready(function(){
+    var state = "";
     $("#logs").click(function(){
         if($("div.history_list").css("display") == "none") {
+            state = "open";
             if($("div.config-container").css("display") != "none") {
                 $("#options").css({backgroundColor:'white'}).css({color:'initial'});
                 $("div.config-container").slideUp(100);
                 $("div.config-header").delay(150).qcss({display:'none'})   
             }
-
             $(this).css({backgroundColor:bgColour}).css({color:'white'});
             $("div.history_header").css({display:'flex'}).css({marginTop:'0px'}).css({width:'60vw'}).css({height:'63.56px'});
             $("div.history_header").css({fontWeight:'bold'}).css({fontSize:'16px'}).css({border:'none'}).css({borderBottom:'thin solid #000000'});
             $("div.history_header").text("Logs");
             $("div.history_list").slideToggle('fast');
         } else {
+            state = "close";
             $(this).css({backgroundColor:'white'}).css({color:'initial'});
             $("div.history_list").slideUp(100);
-            $("div.history_header").delay(150).qcss({display:'none'})
+            $("div.history_header").delay(150).qcss({display:'none'});
             /*$("div.history_header").delay(125).qcss({height:'0x'}).qcss({width:'0px'}).qcss({marginTop:'0px'});*/
             /*$("div.history_header").delay(25).qcss({border:'thin solid #000000'}).qcss({fontWeight:''}).qcss({fontSize:'13.3333px'}).qtext("View Logs");*/
         }
+        toggle_ref(state);
     });
 
     $("#options").click(function(){
+        var state = "";
         if($("div.config-container").css("display") == "none") {
+            state = "open";
             if($("div.history_list").css("display") != "none") {
                 $("#logs").css({backgroundColor:'white'}).css({color:'initial'});
                 $("div.history_list").slideUp(100);
                 $("div.history_header").delay(150).qcss({display:'none'})   
             }
-
             $(this).css({backgroundColor:bgColour}).css({color:'white'});
             $("div.config-header").css({display:'flex'}).css({marginTop:'0px'}).css({width:'60vw'}).css({height:'63.56px'});
             $("div.config-header").css({fontWeight:'bold'}).css({fontSize:'16px'}).css({border:'none'}).css({borderBottom:'thin solid #000000'});
             $("div.config-header").text("Configuration");
             $("div.config-container").slideToggle('fast');
         } else {
+            state = "close";
             $(this).css({backgroundColor:'white'}).css({color:'initial'});
             $("div.config-container").slideUp(100);
-            $("div.config-header").delay(150).qcss({display:'none'})
+            $("div.config-header").delay(150).qcss({display:'none'});
         }
+        toggle_ref(state);
     });
 
     $("form[name='regex_form']").on('submit', initialize_generator);
@@ -83,6 +89,18 @@ $(document).ready(function(){
             }
         }
     });
+
+    function toggle_ref(state) {
+        if($("div.ref-container").css("display") == "none" && state == "close") {
+            $("div.ref-header").css({display:'flex'}).css({marginTop:'0px'}).css({width:'60vw'}).css({height:'63.56px'});
+            $("div.ref-header").css({fontWeight:'bold'}).css({fontSize:'16px'}).css({border:'none'}).css({borderBottom:'thin solid #000000'});
+            $("div.ref-header").text("Reference");
+            $("div.ref-container").slideToggle('fast');
+        } else if(state == "open") {
+            $("div.ref-container").slideUp(100);
+            $("div.ref-header").delay(150).qcss({display:'none'})       
+        }
+    }
 
     function open_log(parent, element) {
         close_log(parent, '.history_record'); // close all logs that are currently open
